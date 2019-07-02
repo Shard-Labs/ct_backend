e5fc20a533496774a6d48659546e9044fc2f30fc
mario <tadic.mario@gmail.com>
2019-07-02 23:36:38 +0200

    fix on database settings

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = {
     host: config.get('db.host'),
     dialect: 'mysql'
   },
-  /*test: {
+  test: {
     username: 'database_test',
     password: null,
     database: 'database_test',
@@ -17,15 +17,10 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        ca: fs.readFileSync(__dirname + '/mysql-ca-master.crt')
-      }
-    }
-  }*/
+    username: config.get('db.username'),
+    password: config.get('db.password'),
+    database: config.get('db.database'),
+    host: config.get('db.host'),
+    dialect: 'mysql'
+  }
 };
