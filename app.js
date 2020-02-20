48a405a0b050495c2834b7868fdc3b7843204cef
mario <tadic.mario@gmail.com>
2020-02-20 22:54:12 +0100

    increase limit in express body

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const feedbacksRouter = require('./routes/feedbacks');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
